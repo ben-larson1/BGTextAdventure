e8b5f7a8406e4e71d18087d7cab545fe628deb00
tsuna <tsuna@DESKTOP-GR00BBE>
2017-05-05 12:20:47 -0400

    added some parameters to clusterable

--- src/Clusterable.java
+++ src/Clusterable.java
@@ -1,4 +1,6 @@
 
 public interface Clusterable {
+	boolean isClustered;
+	int clusterSize;//0=not (1 tile), 1=small (2-3 tiles), 2=medium (4-5 tiles), 3=large (6 tiles)
 	
 }
--- src/Swamp.java
+++ src/Swamp.java
@@ -1,4 +1,5 @@
 
 public class Swamp extends Tile implements Clusterable {
-
+	private boolean isClustered;
+	private int clusterSize;
 }
