70f4c08d2b49b313450853f50f11d1091be268a9
tsuna <tsuna@DESKTOP-GR00BBE>
2017-05-05 12:15:18 -0400

    Added the general class 'Structure' and added its subclass 'Dungeon'

--- src/Town.java
+++ src/Town.java
@@ -1,5 +1,5 @@
 
 public class Town extends Tile implements Clusterable{//not sure whether i want it structurally generated or clusterable, but
-								//for now it is Clusterable
+													  //for now it is Clusterable
 
 }
